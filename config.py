7e7db4c6e729e48a44010843c1e2c9fb1f4ff945
UrMokn <urmoknmc@gmail.com>
2025-04-03 09:43:39 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# 全体
+TOKEN = "TOKEN貼り付け"
+LOG_CHANNEL_ID = "ログチャンネルのID"
+ADMIN_ROLE_ID = "管理者ロールのID"
+SERVER_NOTICE_ROLE_ID = "通知ロールID"
+# データファイル
+POSTED_LINKS_FILE = "data/posted_links.json"
+# 認証
+REGULAR_ROLE_ID = "認証済みロールのID"
+# チケット
+CATEGORY_SUPPORT_ID = "サポートチケットのカテゴリID"
+CATEGORY_APPEAL_ID = "報告のカテゴリID"
+CATEGORY_YT_ID = "YT申請チケットのカテゴリID"
+# YouTube通知
+YOUTUBE_NOTICE_CHANNEL_ID = "youTubeの通知をおこなうDiscordチャンネルID"
+YOUTUBE_CHANNEL_ID = "youTubeのチャンネルID"
+YOUTUBE_CHECK_INTERVAL = 300
